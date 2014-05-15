12d748544caf210812214023cdbb59b50fae8d72
Dewitt Buckingham <dj729@comcast.net>
2014-05-15 08:33:38 -0700

    Cleanup

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var PORT = 7029;
-var APPNAME = "Hermes";
+var APPNICK = "Hermes";
 
 //Set Views
 app.set('views', __dirname + '/templates');
@@ -17,27 +17,34 @@ var io = require('socket.io').listen(app.listen(PORT));
 io.set('transports', ['xhr-polling']);
 console.log('Listening on port ' + PORT);
 
+//Event Callbacks
+
+function onSend(data) {
+    var message = new Message(data.content, data.user).send();
+}
+
+function onJoin(data) {
+    var join = new Message(data + " has joined the chat.").send();
+}
+
+function onDisconnect() {
+    var leaving = new Message("Someone has left.").send();
+}
+
 //Event Handlers
 io.sockets.on('connection', function(socket) {
-    welcome = new Message("Welcome to the ZeroDae chat service.", APPNAME);
-    socket.emit('message', welcome );
-    socket.on('send', function(data) {
-        message = new Message(data.content, data.user);
-        io.sockets.emit('message', message);
-    });
-    socket.on('join', function(data) {
-        join = new Message(data + " has joined the chat.", APPNAME);
-        io.sockets.emit('message', join);
-    });
-    socket.on('disconnect', function() {
-        leaving = new Message("Someone has left.", APPNAME);
-        io.sockets.emit('message', leaving);
-    });
+    var welcome = new Message("Welcome to the ZeroDae chat service.");
+    socket.emit('message', welcome);
+    socket.on('send', onSend);
+    socket.on('join', onJoin);
+    socket.on('disconnect', onDisconnect);
 });
-
-
 
 function Message(content, user) {
     this.content = content;
-    this.user = user;
+    this.user = user || APPNICK;
+}
+
+Message.prototype.send = function() {
+    io.sockets.emit('message', this);
 }
