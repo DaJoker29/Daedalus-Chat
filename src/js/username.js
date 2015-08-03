f8a2fe2b8a071d287050682f4805ea2d94a7b466
Dewitt Buckingham <dj729@comcast.net>
2015-08-02 18:04:53 -0700

    Username system
    Surprisingly more complicated than I thought but it's working now thanks
    to Angular and ngStorage.

---
+++ src/js/username.js
@@ -0,0 +1,55 @@
+/*
+    Username module.
+    Function: Checks to see if there is a username saved locally. If so, it applies it. Otherwise, it generates a random one and applies that.
+
+ */
+// $(function() {
+//     var username;
+//     var input = $('#nickname');
+//     var btn = $('#nicknameBtn');
+//     var form = $('#nicknames');
+
+//     var generateUsername = function () {
+//         return 'Guest' + Math.floor(Math.random() * 100000) + 1;
+//     };
+
+//     var set = function ( str ) {
+//         username = window.localStorage.HermesName = str;
+//     };
+
+//     var display = function() {
+//         input.val( username );
+//     };
+
+//     // Check if username exists
+//     if(window.localStorage.HermesName) {
+//         username = window.localStorage.HermesName;
+//     } else {
+//         set( generateUsername() );
+//     }
+//     display();
+
+//     // Listen for change
+//     $('#nicknames').submit( function ( e ) {
+//         var value = e.target.value;
+
+//         console.log(e);
+//         if( value !== username ) {
+//             set( value );
+//         }
+//     });
+// })();
+// 
+var Hermes = angular.module ('Hermes', ['ngStorage']);
+
+Hermes.controller('Ctrl', function ($scope, $localStorage, $sessionStorage) {
+
+    // Link local storage
+    $scope.$storage = $localStorage;
+
+    if($localStorage.HermesName) {
+        $scope.nickname = $localStorage.HermesName;
+    } else {
+        $scope.nickname = $localStorage.HermesName = 'Guest' + (Math.floor(Math.random() * 10000) + 1);
+    }
+});
