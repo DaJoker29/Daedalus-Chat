5692d236b741a0379ca634c0ee39eb7aa348e368
Dewitt Buckingham <dj729@comcast.net>
2015-08-02 18:48:58 -0700

    Messaging
    Built the messaging framework. Now to add timestamps and order get them
    to display nicely.

--- src/js/username.js
+++ src/js/username.js
@@ -40,11 +40,11 @@
 //     });
 // })();
 // 
-var Hermes = angular.module ('Hermes', ['ngStorage']);
+var Hermes = angular.module ('Hermes', ['ngStorage', 'firebase']);
 
-Hermes.controller('Ctrl', function ($scope, $localStorage, $sessionStorage) {
+Hermes.controller('Ctrl', function ($scope, $localStorage, $firebaseArray) {
 
-    // Link local storage
+    /* Username Code */
     $scope.$storage = $localStorage;
 
     if($localStorage.HermesName) {
@@ -52,4 +52,20 @@ Hermes.controller('Ctrl', function ($scope, $localStorage, $sessionStorage) {
     } else {
         $scope.nickname = $localStorage.HermesName = 'Guest' + (Math.floor(Math.random() * 10000) + 1);
     }
+
+    /* Messaging Code */
+
+    var ref = new Firebase('https://glaring-torch-2044.firebaseio.com/messages');
+
+    $scope.messages = $firebaseArray(ref);
+
+    $scope.addMessage = function() {
+        $scope.messages.$add({
+            nick: $scope.nickname,
+            message: $scope.messageText
+        });
+
+        $scope.messsageText = '';
+        $('#message').val('');
+    };
 });
