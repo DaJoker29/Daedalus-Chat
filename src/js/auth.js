91c7a8c9df2f38ebdaff9b879adea93050673792
Dewitt Buckingham <dj729@comcast.net>
2015-08-06 20:36:37 -0700

    Integrate Google with nickname
    Now the username is automatically pulled from the authenticated Google
    account.

--- src/js/auth.js
+++ src/js/auth.js
@@ -4,19 +4,44 @@
 
  */
 $(function() {
-    var fbLogin = function () {
-        var ref = new Firebase('https://glaring-torch-2044.firebaseio.com');
+    var ref = new Firebase('https://glaring-torch-2044.firebaseio.com');
 
-        ref.authWithOAuthRedirect('google', function (error, authData) {
-            if(error) {
-                console.log('Login Failed.', error);
-            } else {
-                console.log('Authenticated successfully:', authData);
+    var setName = function ( authData ) {
+        var scope = angular.element($('#messages')).scope();
+        scope.$apply(function() {
+            if(!authData) {
+                scope.nick = 'Guest ' + Math.floor((Math.random() * 10000) + 1);
+            } else if(authData.google) {
+                scope.nick = authData.google.displayName;
             }
         });
     };
 
-    $('#fbLogin').on('click', function() {
-        fbLogin();
+    $('#google').click(function() {
+        ref.authWithOAuthPopup('google', function (error, authData) {
+            if(error) {
+                console.log('Login Failed.', error);
+            } else {
+                console.log('Successful Authentication:', authData);
+            }
+        });
+    });
+
+    $('#logout').click(function() {
+        ref.unauth();
+    });
+
+    ref.onAuth(function() {
+        var authData = ref.getAuth();
+        setName( authData );
+
+        if (authData) {
+            console.log(authData.uid + ' is logged in with ' + authData.provider);
+            $('#unauthenticated').hide();
+            $('#authenticated').removeClass('hide');
+        } else {
+            $('#unauthenticated').show();
+            $('#authenticated').addClass('hide');
+        }
     });
 });
--- src/js/messageCtrl.js
+++ src/js/messageCtrl.js
@@ -5,7 +5,7 @@
  */
 angular
     .module('Hermes')
-    .controller('MessageCtrl', function ( $scope, $rootScope, $firebaseArray ) {
+    .controller('MessageCtrl', function ( $scope, $firebaseArray ) {
         var ref = new Firebase('https://glaring-torch-2044.firebaseio.com/messages');
 
         $scope.messages = $firebaseArray(ref.orderByChild('timestamp').limitToLast(7) );
@@ -16,7 +16,7 @@ angular
                 return;
             } else {
                 $scope.messages.$add({
-                    nick: $rootScope.nickname,
+                    nick: $scope.nick,
                     timestamp: Firebase.ServerValue.TIMESTAMP,
                     message: $scope.messageText
                 });
