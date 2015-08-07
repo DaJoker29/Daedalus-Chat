dadabfd40afa9c769499b812a4e04da71db995be
Dewitt Buckingham <dj729@comcast.net>
2015-08-06 21:24:29 -0700

    Auth Data
    * Add profile pictures
    * Add provider model

--- src/js/auth.js
+++ src/js/auth.js
@@ -5,14 +5,33 @@
  */
 $(function() {
     var ref = new Firebase('https://glaring-torch-2044.firebaseio.com');
+    var scope = angular.element($('#messages')).scope();
 
     var setName = function ( authData ) {
-        var scope = angular.element($('#messages')).scope();
         scope.$apply(function() {
             if(!authData) {
                 scope.nick = 'Guest ' + Math.floor((Math.random() * 10000) + 1);
             } else if(authData.google) {
                 scope.nick = authData.google.displayName;
+            }
+        });
+    };
+
+    var setURL = function ( authData ) {
+        scope.$apply(function() {
+            if(!authData) {
+                scope.url = 'http://placehold.it/100x100';
+            } else if (authData.google) {
+                scope.url = authData.google.profileImageURL;
+            }
+        });
+    };
+    var setProvider = function ( authData ) {
+        scope.$apply(function() {
+            if(!authData) {
+                scope.provider = null;
+            } else if (authData.google) {
+                scope.provider = authData.provider;
             }
         });
     };
@@ -34,6 +53,8 @@ $(function() {
     ref.onAuth(function() {
         var authData = ref.getAuth();
         setName( authData );
+        setURL( authData );
+        setProvider ( authData );
 
         if (authData) {
             console.log(authData.uid + ' is logged in with ' + authData.provider);
